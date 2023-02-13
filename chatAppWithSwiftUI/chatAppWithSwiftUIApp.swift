ea973f6468d442276feab0ad432e93d82e89ae73
ismv00 <ismv00@icloud.com>
2023-02-12 20:09:33 -0400

    feat:firts-commit

---
+++ chatAppWithSwiftUI/chatAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppWithSwiftUIApp.swift
+//  chatAppWithSwiftUI
+//
+//  Created by Igor S. Menezes on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct chatAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
